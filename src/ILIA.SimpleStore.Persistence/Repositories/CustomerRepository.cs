0db75b9428885ad4461f0a088133a7d3aa624da2
Jesse Junior <jesse@4d.bsb.br>
2022-04-07 20:57:05 -0300

    Implemented CustomerRepository

---
+++ src/ILIA.SimpleStore.Persistence/Repositories/CustomerRepository.cs
@@ -0,0 +1,23 @@
+﻿using ILIA.SimpleStore.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ILIA.SimpleStore.Persistence.Repositories
+{
+    public class CustomerRepository : Repository<Customer>, ICustomerRepository
+    {
+        public CustomerRepository(SimpleStoreContext context) : base(context)
+        {
+        }
+
+        public async Task<Customer> GetCustomersAndRelatedOrdersById(Guid id)
+        {
+            return await Entities.Include(c => c.Orders).FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+    }
+}
