d63092f6f8e0eb9462e87d6ca5e012a753862b1d
Jesse Junior <jesse@4d.bsb.br>
2022-04-07 18:57:46 -0300

    added mappings and dbcontext

---
+++ src/ILIA.SimpleStore.Persistence/Mappings/OrderMapping.cs
@@ -0,0 +1,25 @@
+﻿using ILIA.SimpleStore.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ILIA.SimpleStore.Persistence.Mappings
+{
+    internal class OrderMapping : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.HasKey(o => o.Id);
+
+            builder.Property(o => o.Price).HasPrecision(2).IsRequired();
+
+            builder.Property(o => o.CreatedAt).IsRequired();
+            
+            builder.ToTable("Orders");
+        }
+    }
+}
---
+++ src/ILIA.SimpleStore.Persistence/SimpleStoreContext.cs
@@ -0,0 +1,27 @@
+﻿using ILIA.SimpleStore.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ILIA.SimpleStore.Persistence;
+
+public class SimpleStoreContext : DbContext
+{
+    public SimpleStoreContext(DbContextOptions<SimpleStoreContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Customer> Products { get; set; }
+    public DbSet<Order> Orders { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(SimpleStoreContext).Assembly);
+    }
+    
+ 
+}
