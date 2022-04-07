738599a670b3bfa36a1912efd9074f5dd177f5df
Jesse Junior <jesse@4d.bsb.br>
2022-04-07 11:52:55 -0300

    Created Entityes

---
+++ src/ILIA.SimpleStore.Domain/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ILIA.SimpleStore.Domain;
+
+public class Customer
+{
+
+    public Customer(string name, string email, IEnumerable<Order> orders)
+    {
+        Name = name;
+        Email = email;
+        Orders = orders;
+    }
+
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public IEnumerable<Order> Orders { get; set; }
+}
+
---
+++ src/ILIA.SimpleStore.Domain/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace ILIA.SimpleStore.Domain;
+
+public class Order 
+{
+    public decimal Price { get; set; }
+    public DateTime CreatedAt { get; set; }
+    //TDOO: IMPLEMENT STATUS
+}
+
