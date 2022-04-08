91b0d3176aa3402f037d2a97cb60b1eb4ef08329
Jesse Junior <jesse@4d.bsb.br>
2022-04-07 21:37:27 -0300

    Created Extention Method for removing Sentences in a string

---
+++ src/ILIA.SimpleStore.API/Extentions/StringExtentions.cs
@@ -0,0 +1,15 @@
+﻿namespace ILIA.SimpleStore.API.Extentions
+{
+    public static class StringExtentions
+    {
+        public static string RemoveSentence(this string text, string textToRemove)
+        {
+            if (String.IsNullOrEmpty(textToRemove))
+            {
+                return text;
+            }
+            
+            return text.Replace(textToRemove, "");
+        }
+    }
+}
