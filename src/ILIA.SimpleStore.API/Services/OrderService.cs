4d0680760eacf4b74efca3f3e4b1781986211895
Jesse Junior <jesse@4d.bsb.br>
2022-04-08 10:56:08 -0300

    Created OrderService for adding orders

---
+++ src/ILIA.SimpleStore.API/Services/OrderService.cs
@@ -0,0 +1,37 @@
+﻿using ILIA.SimpleStore.API.Models;
+using ILIA.SimpleStore.Domain;
+using ILIA.SimpleStore.Persistence;
+
+namespace ILIA.SimpleStore.API.Services
+{
+    public interface IOrderService
+    {
+        public Task<(Order orderCreated, IEnumerable<string> Errors)> CreateOrder(Order order, Guid customerId);
+    }
+
+    public class OrderService : IOrderService
+    {
+        private ICustomerRepository _customerRepository;
+
+        public OrderService(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+        
+        public async Task<(Order? orderCreated, IEnumerable<string> Errors)> CreateOrder(Order order, Guid customerId)
+        {
+            var customer = await _customerRepository.GetById(customerId);
+
+            if (customer is null) return (null, new string[] { "Customer Not Found" });
+
+            customer.AddOrder(order);
+
+            _customerRepository.Update(customer);
+
+            await _customerRepository.Commit();
+
+            return (order, null);
+
+        }
+    }
+}
