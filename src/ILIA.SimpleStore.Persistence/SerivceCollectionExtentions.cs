54db099f8ab4fa9901095dd8aee6dd00579932b1
Jesse Junior <jesse@4d.bsb.br>
2022-04-07 23:01:16 -0300

    Created Extention method for configuring persistence

---
+++ src/ILIA.SimpleStore.Persistence/SerivceCollectionExtentions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ILIA.SimpleStore.Domain;
+using ILIA.SimpleStore.Persistence.Repositories;
+
+namespace ILIA.SimpleStore.Persistence
+{
+    public static class SerivceCollectionExtentions 
+    {
+        public static IServiceCollection AddDefaultPersisntece(this IServiceCollection services)
+        {
+            //TOOD: ADD LOAD TEST
+            //TODO: CHANGE TO REAL DB
+            services.AddDbContext<SimpleStoreContext>(opt => opt.UseInMemoryDatabase("temporaryDb"));
+            services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
+            services.AddTransient<ICustomerRepository, CustomerRepository>();
+            return services;
+
+        }
+    }
+}
