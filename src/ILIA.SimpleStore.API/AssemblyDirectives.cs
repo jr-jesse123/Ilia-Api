44b3980c895ad7a56e7dfa36d1549868fb998038
Jesse Junior <jesse@4d.bsb.br>
2022-04-07 21:19:10 -0300

    Added Assembly directives

---
+++ src/ILIA.SimpleStore.API/AssemblyDirectives.cs
@@ -0,0 +1,4 @@
+﻿
+//THIS DIRECTIVE MAKES DE IMPLIED INTERNAL PROGRAM CLASS VISIBLE TO THE TEST PROOJECT
+using System.Runtime.CompilerServices;
+[assembly:InternalsVisibleTo("ILIA.SimpleStore.IntegrationTests")]
