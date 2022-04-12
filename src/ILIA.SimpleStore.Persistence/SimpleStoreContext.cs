f7cdb7ff694221a857487c36ee23bfbb2095340c
Jesse Junior <jesse@4d.bsb.br>
2022-04-12 15:33:13 -0300

    added mailservice hooks into dbcontext savechanges

--- src/ILIA.SimpleStore.Persistence/SimpleStoreContext.cs
+++ src/ILIA.SimpleStore.Persistence/SimpleStoreContext.cs
@@ -1,4 +1,5 @@
-﻿using ILIA.SimpleStore.Domain;
+﻿using ILIA.SimpleStore.API.Services;
+using ILIA.SimpleStore.Domain;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -10,11 +11,16 @@ namespace ILIA.SimpleStore.Persistence;
 
 public class SimpleStoreContext : DbContext
 {
-    public SimpleStoreContext(DbContextOptions<SimpleStoreContext> options) : base(options)
+    private readonly IMailService mailService;
+
+    public SimpleStoreContext(
+        DbContextOptions<SimpleStoreContext> options, 
+        IMailService mailService) : base(options)
     {
+        this.mailService = mailService;
     }
 
-    public DbSet<Customer> Products { get; set; }
+    public DbSet<Customer> Customers { get; set; }
     public DbSet<Order> Orders { get; set; }
 
 
@@ -22,6 +28,52 @@ public class SimpleStoreContext : DbContext
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(SimpleStoreContext).Assembly);
     }
+
     
- 
+
+    public override int SaveChanges()
+    {
+        
+        var addedCustomers = GetAddedCustomers();
+
+        var output = base.SaveChanges();
+
+        foreach (var customer in addedCustomers)
+        {
+            mailService.SendMail(customer.Email, "Welcome to ILIA.SimpleStore", "Welcome to ILIA.SimpleStore");
+        }
+
+
+        return output;
+    }
+
+
+    public async override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken))
+    {
+        var addedCustomers = GetAddedCustomers();
+
+        var output = await base.SaveChangesAsync(cancellationToken);
+
+        foreach (var customer in addedCustomers)
+        {
+            mailService.SendMail(customer.Email, "Welcome to ILIA.SimpleStore", "Welcome to ILIA.SimpleStore");
+        }
+
+        return output;
+    }
+
+    private Customer[] GetAddedCustomers()
+    {
+        this.ChangeTracker.DetectChanges();
+        var addedCustomers = this.ChangeTracker.Entries()
+                    .Where(t => t.State == EntityState.Added)
+                    .Select(t => t.Entity)
+                    .Where(entity => entity.GetType() == typeof(Customer))
+                    .Cast<Customer>()
+                    .ToArray();
+
+        return addedCustomers;
+
+
+    }
 }
