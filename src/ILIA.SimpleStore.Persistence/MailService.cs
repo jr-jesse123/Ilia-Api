9566185031531b6cab741d38a83bea2864a960e3
Jesse Junior <jesse@4d.bsb.br>
2022-04-12 15:32:59 -0300

    implemented mailservice

---
+++ src/ILIA.SimpleStore.Persistence/MailService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Options;
+using System.Net.Mail;
+
+namespace ILIA.SimpleStore.API.Services
+{
+    
+    public class MailOptions
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+    }
+
+    public interface IMailService
+    {
+        void SendMail(string to, string subject, string body);
+    }
+    public class MailService : IMailService
+    {
+        private SmtpClient client;
+        public MailService(IOptions<MailOptions> options)
+        {
+            _ = options.Value.Host ?? throw new System.ArgumentNullException("The Host Need to be setted");
+            if (options.Value.Port == 0)
+                throw new ArgumentException("The port needs to be setted");
+
+            client = new SmtpClient
+            {
+                Host = options.Value.Host,
+                Port = options.Value.Port,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false
+            };
+            
+        }
+        
+
+        public void SendMail(string to, string subject, string body)
+        {
+            client.Send("Isla@Isla.com", to, subject, body);
+        }
+    }
+}
